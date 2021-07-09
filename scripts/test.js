222bfadfd77728b2c364d97d186ba63e07ea6637
Jonathan Langlois <jon@button.is>
2021-07-09 15:03:14 -0700

    test

--- scripts/test.js
+++ scripts/test.js
@@ -1,12 +1,12 @@
 // This module runs in GitHub Action `github-script`
 // see https://github.com/actions/github-script#run-a-separate-file-with-an-async-function
 const axios = require('axios');
-const axiosConfig = { headers: { Authorization: "secret" } };
+const axiosConfig = { headers: { Authorization: process.env.GH_SECRET } };
 const url = "https://cttuyb3bsf.execute-api.ca-central-1.amazonaws.com/test/actions/";
 
 module.exports = async ({ github, context }) => {
   const { payload } = context;
-  console.log(process.env.GH_SECRET, 'is env');
+  console.log(process.env.GH_SECRET.length);
 
   axios.put(url, { prNumber: 1, prSuccess: true, id: 1 }, axiosConfig);
 
