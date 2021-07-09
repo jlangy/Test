200b71cb3ce57848f88aec29d5036dcbe9305acb
Jonathan Langlois <jon@button.is>
2021-07-09 10:23:20 -0700

    test

--- scripts/test.js
+++ scripts/test.js
@@ -2,5 +2,5 @@
 // see https://github.com/actions/github-script#run-a-separate-file-with-an-async-function
 module.exports = async ({ github, context }, secret) => {
   const { payload } = context;
-  console.log(process.env, 'is env');
+  console.log(process.env.GH_SECRET, 'is env');
 };
