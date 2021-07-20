dfa1d362ddcb66e4f1329f1f69fa456763963f17
Jonathan Langlois <jon@button.is>
2021-07-20 14:04:44 -0700

    test

--- scripts/test.js
+++ scripts/test.js
@@ -1,9 +1,3 @@
-const fs = require('fs');
-const _ = require('lodash');
-const generateClients = require('./generate-clients');
-const axios = require('axios');
-const API_URL = 'https://gcp9dahm4c.execute-api.ca-central-1.amazonaws.com/test/actions?status=create';
-const axiosConfig = { headers: { Authorization: process.env.GH_SECRET } };
 
 // This module runs in GitHub Action `github-script`
 // see https://github.com/actions/github-script#run-a-separate-file-with-an-async-function
