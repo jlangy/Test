3c76efad463dbf0a3b8624eaedd6344d09fa866d
Jonathan Langlois <jon@button.is>
2021-07-30 13:37:31 -0700

    test

--- scripts/test.js
+++ scripts/test.js
@@ -2,7 +2,7 @@
 // This module runs in GitHub Action `github-script`
 // see https://github.com/actions/github-script#run-a-separate-file-with-an-async-function
 module.exports = async ({ github, context }) => {
-  const result = await github.rest.pulls.merge({
+  const result = await github.pulls.merge({
     owner: 'jlangy',
     repo: 'Test',
     pull_number: 1,
