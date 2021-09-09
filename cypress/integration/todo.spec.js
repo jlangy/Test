f08cbf259485b1f1e2a3794ff7d30fc56f79cb8b
Jonathan Langlois <jon@button.is>
2021-09-09 12:00:20 -0700

    test

---
+++ cypress/integration/todo.spec.js
@@ -0,0 +1,122 @@
+/// <reference types="cypress" />
+
+const CONTAINS_DASHBOARD_LINK = 'My Dashboard';
+const CONTAINS_REQUEST_INTEGRATION = 'Request Integration';
+const CONTAINS_NOT_PROJECT_LEAD_TEXT =
+  'We can only process access requests submitted by product owners, project admin or team leads.';
+const CONTAINS_NEXT_BUTTON = 'Next';
+const CONTAINS_SUBMIT_BUTTON = 'Submit';
+const CONTAINS_DELETE_BUTTON = 'Delete';
+const CONTAINS_INSTALLATION_JSON = 'Installation JSON';
+
+const GET_PROJECT_LEAD_FIELDS = '#root_projectLead';
+const GET_NEW_TO_SSO_FIELDS = '#root_newToSso';
+const GET_PROJECT_NAME = '#root_projectName';
+const GET_PUBLIC_ACCESS = '#root_publicAccess';
+const GET_DEV_REDIRECT_URIS = '#root_devValidRedirectUris_0';
+const GET_DEV_REDIRECT_URIS_1 = '#root_devValidRedirectUris_1';
+const GET_TEST_REDIRECT_URIS = '#root_testValidRedirectUris_0';
+const GET_PROD_REDIRECT_URIS = '#root_prodValidRedirectUris_0';
+const GET_AGREE_WITH_TERMS = '#root_agreeWithTerms';
+
+const projectName = 'cypress test';
+
+const answerRadio = (getBy, answer) => {
+  cy.get(getBy).within(() => {
+    cy.contains(answer).click();
+  });
+};
+
+const validateSubmission = (filename) => {
+  cy.contains('tr.active', projectName);
+  cy.contains('tr.active', 'Submitted');
+  cy.contains(CONTAINS_INSTALLATION_JSON, {timeout: 600000}).screenshot(filename);
+};
+
+beforeEach(() => {
+  cy.visit('https://bcgov.github.io/sso-terraform-dev/');
+  cy.window().then((win) => {
+    win.sessionStorage.setItem('tokens', Cypress.env('TOKEN'));
+  });
+});
+
+describe('example to-do app', () => {
+
+  it('should be logged in', () => {
+    cy.contains('Log in')
+    cy.screenshot('test')
+  });
+
+  // it('should require user to be project lead', () => {
+  //   cy.contains(CONTAINS_DASHBOARD_LINK).click();
+  //   cy.contains(CONTAINS_REQUEST_INTEGRATION).click();
+  //   answerRadio(GET_PROJECT_LEAD_FIELDS, 'No');
+  //   cy.contains(CONTAINS_NOT_PROJECT_LEAD_TEXT);
+  // });
+
+  // it('should allow the user to create a request', () => {
+  //   cy.contains(CONTAINS_DASHBOARD_LINK).click();
+  //   cy.contains(CONTAINS_REQUEST_INTEGRATION).click();
+
+  //   // Page 1
+  //   answerRadio(GET_PROJECT_LEAD_FIELDS, 'Yes');
+  //   answerRadio(GET_NEW_TO_SSO_FIELDS, 'Yes');
+  //   cy.get(GET_PROJECT_NAME).type(projectName);
+  //   cy.contains('button', CONTAINS_NEXT_BUTTON).click();
+
+  //   // Page 2
+  //   answerRadio(GET_PUBLIC_ACCESS, 'Public');
+  //   cy.get(GET_DEV_REDIRECT_URIS).type('http://cypress');
+  //   cy.get(GET_TEST_REDIRECT_URIS).type('http://cypress');
+  //   cy.get(GET_PROD_REDIRECT_URIS).type('http://cypress');
+  //   cy.contains('button',CONTAINS_NEXT_BUTTON).click();
+
+  //   // Page 3
+  //   cy.get(GET_AGREE_WITH_TERMS).click();
+  //   cy.contains('button', CONTAINS_NEXT_BUTTON).click();
+
+  // // Submit
+  //   cy.contains('button', CONTAINS_SUBMIT_BUTTON).click();
+  //   cy.contains('.pg-modal-content button', CONTAINS_SUBMIT_BUTTON).click();
+
+  //   validateSubmission('created-request');
+
+  // });
+
+  // it('Should allow the user to update their request', () => {
+  //   cy.contains(CONTAINS_DASHBOARD_LINK).click();
+  //   cy.contains(projectName).click();
+  //   cy.contains(projectName)
+  //     .parent('tr')
+  //     .within(() => {
+  //       cy.get('.fa-edit').click();
+  //     });
+
+  //   cy.contains('Redirect URIs').click();
+  //   cy.contains('Add another URI');
+
+  //   cy.contains('legend', 'Development')
+  //     .parent('div')
+  //     .within(() => {
+  //       cy.contains('Add another URI').click();
+  //     });
+
+  //   cy.get(GET_DEV_REDIRECT_URIS_1).type('http://cypress-2');
+  //   cy.contains('button', CONTAINS_SUBMIT_BUTTON).click();
+
+  //   validateSubmission();
+  // });
+
+  // it('Should allow the user to delete their request', () => {
+  //   cy.contains(CONTAINS_DASHBOARD_LINK).click();
+  //   cy.contains(projectName).click();
+  //   cy.contains(projectName)
+  //     .parent('tr')
+  //     .within(() => {
+  //       cy.get('.fa-trash').click();
+  //     });
+
+  //   cy.contains('.pg-modal-content button', CONTAINS_DELETE_BUTTON).click();
+  //   cy.contains(projectName).should('not.exist')
+  // });
+});
